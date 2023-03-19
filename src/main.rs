fc1a5826d22e493a6cae849563ff1765e9892e87
aesedr1@gmail.com <aesedr1@gmail.com>
2023-03-19 15:05:10 +0100

    q

--- src/main.rs
+++ src/main.rs
@@ -485,33 +485,6 @@ impl K{
 struct Friendly;
 
 #[derive(Component, Default)]
-struct Vehicle{
-    pl:     Place,
-    entity: Option<Entity>,
-    camera_y: f32,
-    camera_x: f32,
-}
-impl Vehicle{
-    fn new()
-    -> Self{
-        Vehicle{
-            pl: Place::new(),
-            entity: None,
-            camera_y: 0.0,
-            camera_x: 0.0,
-        }
-    }
-    fn forward( &self )
-    -> Vec3{
-        Vec3::new( self.camera_y.cos() * self.camera_x.cos(), self.camera_x.sin(), - self.camera_y.sin() * self.camera_x.cos())
-    }
-    fn passenger( &self )
-    -> Vec3{
-        Vec3{ x: self.pl.i, y: self.pl.j+1.0, z: self.pl.k }
-    }
-}
-
-#[derive(Component, Default)]
 struct Nmo{
     pl:     Place,
     entity: Option<Entity>,
